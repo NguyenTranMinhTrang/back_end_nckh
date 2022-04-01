996f979570637960cae486b10ed99096843b60a6
PhiHoc <phihoc656621@gmail.com>
2022-04-01 22:36:59 +0700

    db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,15 +1,49 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: 'us-cdbr-east-05.cleardb.net',
+//     user: 'b8068a5f67c0f2',
+//     password: 'c823b7c6',
+//     database: 'heroku_ee6ef0c6e41f314'
+// });
+
+// connection.connect(function (err) {
+//     (err) ? console.log(err) : console.log('Connect successfully');
+// });
+
+var db_config = {
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b8068a5f67c0f2',
     password: 'c823b7c6',
     database: 'heroku_ee6ef0c6e41f314'
-});
+};
 
-connection.connect(function (err) {
-    (err) ? console.log(err) : console.log('Connect successfully');
-});
+var connection;
 
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config);
+    // the old one cannot be reused.
+
+    connection.connect(function (err) {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+        else {
+            console.log('Connect successfully')
+        }
+    });
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
